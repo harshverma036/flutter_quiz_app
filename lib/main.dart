dc485fb46f3a886f106816e7fcb10d77edd6a3ed
harshverma036 <harshverma0362@gmail.com>
2022-04-14 15:44:15 +0530

    stateless widget convert to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,57 @@
+// ignore_for_file: deprecated_member_use, prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void answerQuestions() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questions[questionIndex]);
+  }
+
+  var questions = ["What is your fav color", "What is you fav choclate"];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Quiz App!"),
+          title: Text(
+            "Quiz App!"
+          ),
         ),
-        body: Text("This is default text"),
+        body: Column(children: [
+          Text(questions[questionIndex],),
+          RaisedButton(
+            child: Text("Answer 1"),
+            onPressed: answerQuestions,
+          ),
+          RaisedButton(
+            child: Text("Answer 2"),
+            onPressed: answerQuestions,
+          ),
+          RaisedButton(
+            child: Text("Answer 3"),
+            onPressed: answerQuestions,
+          ),
+          RaisedButton(
+            child: Text("Answer 4"),
+            onPressed: answerQuestions,
+          ),
+        ]),
       ),
     );
   }
